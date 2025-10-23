3730d6f42e9768553a01bbaa4e06f2e616fc7f71
nz-david <oliverspaly@gmail.com>
2025-10-23 17:37:39 -0400

    Added Music

--- scripts/music.gd
+++ scripts/music.gd
@@ -1,10 +1,9 @@
 extends AudioStreamPlayer2D
 const TIME_FOR_ADVENTURE = preload("res://assets/brackeys_platformer_assets/music/time_for_adventure.mp3")
 const WRECKING_BALLMP_3 = preload("res://assets/brackeys_platformer_assets/music/Wrecking_Ballmp3.wav")
+const RESISTANCE_10_12_25_ = preload("uid://dlolv4623plqx")
+const I_STAND_AND_FACE_IT_10_23_2025_ = preload("uid://coe2s52dflbsp")
 
 func switch():
-	if stream==WRECKING_BALLMP_3:
-		stream=TIME_FOR_ADVENTURE
-	else:
-		stream=WRECKING_BALLMP_3
+	stream=RESISTANCE_10_12_25_
 	play(0)
